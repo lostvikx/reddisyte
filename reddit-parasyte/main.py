7c236dbd5bbc41f3e907146213bc1740fcb503ba
Vikram Negi <viknegi0@gmail.com>
2022-07-19 12:54:39 +0530

    main fn

--- reddit-parasyte/main.py
+++ reddit-parasyte/main.py
@@ -1,44 +1,22 @@
 #!/usr/bin/env python3
 
-import requests
-import re
 from reddit import Reddit
 
-def get_reddit_videos(subreddit="TikTokCringe", page_sort="top", post_limit=10):
-  reddit_tiktok_url = f"https://www.reddit.com/r/{subreddit}/{page_sort}.json?limit={post_limit}"
-  headers = {"user-agent": "Linux Machine (Vikram Singh Negi)"}
 
-  res = requests.get(reddit_tiktok_url, headers=headers)
-  try:
-    res.raise_for_status()
-    data = res.json()["data"]
-  except Exception as err:
-    print(f"HTTP Error: {err}")
+def main():
+  # Story: Post title + Top comments (no media)
+  reddit = Reddit(subreddit="AskReddit", num_posts=10)
+  reddit.refine_story_posts()
+  all_posts = reddit.get_all_posts()
 
-  # [child["data"] for child in data["children"]][0]
+  # Test: display post titles
+  # for post in all_posts:
+  #   print(post["title"])
 
-  dt = []
-  valid_fields = ["subreddit", "title", "thumbnail", "url_overridden_by_dest",
-                  "subreddit_id", "author", "url", "media", "is_video"]
-
-  for child in data["children"]:
-    if child["data"]["is_video"]:
-      child_data = {}
-      for key, val in child["data"].items():
-        if key in valid_fields:
-          if key == "media":
-            fallback_url = val["reddit_video"]["fallback_url"]
-            child_data["video_url"] = fallback_url
-            child_data["audio_url"] = re.sub(
-                r"[\w+\/]DASH_(\d+)", "/DASH_audio", fallback_url)
-          else:
-            child_data[key] = val
-      dt.append(child_data)
-  
-  return dt, len(dt)
+  rand_story = reddit.get_random_story_post()
+  # print(rand_story)
+  reddit.extract_comments(rand_story)
 
 
 if __name__ == "__main__":
-  # print(get_reddit_videos())
-  print("who?")
-  Reddit().refine_story_post()
+  main()

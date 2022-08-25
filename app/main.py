68a097d1146bd32ce251f60e6e50d5c34973c031
Vikram Negi <viknegi0@gmail.com>
2022-08-26 00:44:12 +0530

    arg parser implementation, terminal application & story file

--- app/main.py
+++ app/main.py
@@ -1,108 +1,23 @@
 #!/usr/bin/env python3
 
-import asyncio
-import os
-from utils import youtube_video_meta_defaults, print_story
-
-from playwright.async_api import async_playwright
-from reddit import Reddit
-from GoogleTTS.tts import GoogleTTS
-from Playwright.screenshot import Playwright
-from VideoEditor.editor import VideoEditor
-from YTUpload.upload import UploadYT
-
-# TODO: Only titles (no comments) --only-titles
-# NOTE: subreddits = ["Showerthoughts", "AskReddit"]
-
-
-async def run_playwright(url:str, is_nsfw, div_ids:list):
-  async with async_playwright() as p:
-    browser = await p.chromium.launch(headless=True)
-    page = await browser.new_page()
-
-    play = Playwright(page)
-    await play.navigate(url)
-    await play.save_screenshots(div_ids, is_nsfw)
-    await browser.close()
+import story
+from argparse import ArgumentParser
 
 
 def main():
-  dir_path = os.path.dirname(os.path.realpath(__file__))
-  # story: post title + top comments (posts with no media)
-  subreddit = "AskReddit"
-  reddit = Reddit(subreddit=subreddit, content="story", num_posts=10, filter="week")
-  all_posts = reddit.get_all_posts(no_media=True)
-  # Display post titles
-  reddit.display_all_posts_title()
+  parser = ArgumentParser(description="A simple terminal tool to scrape Reddit for content, because good artists borrow, great artists steal.")
+  group = parser.add_mutually_exclusive_group(required=True)
 
-  # Select a post
-  try:
-    post_num = int(input("Post Number: "))
-    post_selected = all_posts[post_num]
-  except:
-    print("Enter a valid number!")
-    exit()
+  # Can ignore this:
+  group.add_argument("-s","--create-story",action="store_true",help="create a video story using reddit comments and use text-to-speech for narration, then upload with minecraft background video")
+  group.add_argument("-v","--short-video",action="store_true",help="simply download & upload a short video, preferably less than 60s")
+  group.add_argument("-c","--compilation-video",action="store_true",help="download a bunch of short videos & compile them together, then upload")
 
-  post_title = post_selected["title"].strip()
-  if post_selected["over_18"]:
-    post_title += " [NSFW]"
+  args = parser.parse_args()
 
-  # Top 20 comments of a particular post
-  comments = reddit.extract_comments(post=post_selected,num_comments=20)
-
-  # Prepare for TTS
-  text_list = [post_selected["title"].strip()]
-
-  div_ids_list = [post_selected["name"]]
-  for com in comments:
-    text_list.append(com["body"])
-    div_ids_list.append(com["name"])
-
-  # Print out the title & its comments
-  print_story(text_list)
-  do = input("Continue? [Y/n] ").lower()
-  if do in ["n", "no"]: exit()
-
-  # sum(audio_clips) <= 40 seconds
-  g_tts = GoogleTTS(text=text_list, limit_duration=40)
-  text_list = g_tts.get_text_list()
-  div_ids_list = div_ids_list[:len(text_list)]
-
-  print(g_tts.get_audio_timestamps())
-
-  # Extracting screenshots from the subreddit
-  asyncio.run(run_playwright(post_selected["url"], post_selected["over_18"], div_ids_list))
-
-  # Creating the final video
-  video = VideoEditor("minecraft_1440p.webm")
-  video.create_short()
-  video.add_screenshots(path=f"{dir_path}/Playwright/temp", timestamps=g_tts.get_audio_timestamps())
-  video.add_tts(path=f"{dir_path}/GoogleTTS/temp")
-  vids = len(os.listdir(f"{dir_path}/../Videos"))
-  video_file = f"{subreddit.lower()}_{vids}.mp4"
-  video.export(video_file)
-
-  # Uploading video to YouTube
-  up = input("Upload video on YouTube? [Y/n] ").lower()
-  if up in ["n", "no"]: exit()
-
-  upload = UploadYT()
-
-  vid_meta = {
-    "file": f"{dir_path}/../Videos/{video_file}",
-    "title": f"{post_title} - r/{subreddit}",
-    "description": youtube_video_meta_defaults["descriptions"],
-    "keywords": youtube_video_meta_defaults["keywords"],
-    "privacy_status": "public"
-  }
-
-  if len(vid_meta["title"]) >= 100:
-    print(f'Title Character Limit!\nTitle: {vid_meta["title"]}')
-    new_vid_title = input("Enter a custom YT title: ")
-    vid_meta["title"] = f"{new_vid_title} - r/{subreddit}"
-
-  youtube = upload.authenticate_service()
-  upload.init_upload(youtube,vid_meta)
+  if args.create_story: story.create_story()
+  elif args.short_video: print("short video fn")
+  elif args.compilation_video: print("compile fn")  
 
 
 if __name__ == "__main__":
